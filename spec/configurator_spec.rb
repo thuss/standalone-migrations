8f86155d7d3f6722a5152bfd68819dfaed7a4c88
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2011-10-31 13:11:24 -0200

    create a skeleton spec for configurator

---
+++ spec/configurator_spec.rb
@@ -0,0 +1,12 @@
+describe Configurator, "which allows define custom dirs and files to work with your migrations" do
+
+  context "without configurator" do
+
+    it "should use default configuration dir (and file)" do
+
+      pending "searches for database.yml on config"
+
+    end
+  end
+
+end
