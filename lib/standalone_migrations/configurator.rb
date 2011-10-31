b9641b96fe7fe47eff6bed1d5892e326be65a837
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2011-10-31 18:37:58 -0200

    now Configurator uses a .standalone_migrations file (yaml format) to extract configs
    doing fallback to defaults n rails

--- lib/standalone_migrations/configurator.rb
+++ lib/standalone_migrations/configurator.rb
@@ -1,6 +1,7 @@
 module StandaloneMigrations
   class Configurator
 
+
     def initialize(options = {})
       defaults = {
         :config       => "config/database.yml",
@@ -8,22 +9,40 @@ module StandaloneMigrations
         :seeds        => "db/seeds.rb",
         :schema       => "db/schema.rb"
       }
+      @options = load_from_file || defaults.merge(options)
     end
 
     def config
-      "config/database.yml"
+      @options[:config]
     end
 
     def migrate_dir
-      "db/migrate"
+      @options[:migrate_dir]
     end
 
     def seeds
-      "db/seeds.rb"
+      @options[:seeds]
     end
 
     def schema
-      "db/schema.rb"
+      @options[:schema]
+    end
+
+    private
+
+    def configuration_file
+      ".standalone_migrations"
+    end
+
+    def load_from_file
+      return nil unless File.exists? configuration_file
+      config = YAML.load( IO.read(configuration_file) ) 
+      {
+        :config       => config[:config][:database],
+        :migrate_dir  => config[:db][:migrate],
+        :seeds        => config[:db][:seeds],
+        :schema       => config[:db][:schema]
+      }
     end
 
   end
--- spec/standalone_migrations/configurator_spec.rb
+++ spec/standalone_migrations/configurator_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'yaml'
 
 module StandaloneMigrations
   describe Configurator, "which allows define custom dirs and files to work with your migrations" do
@@ -46,10 +47,68 @@ module StandaloneMigrations
         configurator.config.should == args[:config]
       end
 
+      it "use custom migrate dir" do
+        configurator.migrate_dir.should == args[:migrate_dir]
+      end
+
+      it "use custom seeds" do
+        configurator.seeds.should == args[:seeds]
+      end
+
+      it "use custom schema" do
+        configurator.schema.should == args[:schema]
+      end
+
     end
 
-    context "using a .standalone_migrations file with configuration" do
-    end
+    context "using a .standalone_migrations file with configurations" do
 
+      before(:all) do
+        @original_dir = Dir.pwd
+        Dir.chdir File.expand_path("../", __FILE__)
+      end
+
+      let(:yaml_hash) do
+        {
+          :db => {
+            :seeds    => "file/db/seeds.rb",
+            :migrate  => "file/db/migrate",
+            :schema   => "file/db/schema.rb"
+          },
+          :config => {
+            :database => "file/config/database.yml"
+          }
+        }
+      end
+
+      let(:configurator) do
+        file = ".standalone_migrations"
+        File.open(file, "w") { |file| file.write(yaml_hash.to_yaml) }
+
+        Configurator.new
+      end
+
+      it "use custom config from file" do
+        configurator.config.should == yaml_hash[:config][:database]
+      end
+
+      it "use custom migrate dir from file" do
+        configurator.migrate_dir.should == yaml_hash[:db][:migrate]
+      end
+
+      it "use custom seeds from file" do
+        configurator.seeds.should == yaml_hash[:db][:seeds]
+      end
+
+      it "use custom schema from file" do
+        configurator.schema.should == yaml_hash[:db][:schema]
+      end
+
+      after(:all) do
+        File.delete ".standalone_migrations"
+        Dir.chdir @original_dir
+      end
+
+    end
   end
 end
