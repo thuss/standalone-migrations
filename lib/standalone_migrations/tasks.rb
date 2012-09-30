7c38f2a1e6b665f58b9c72c9fa5e14d9cc248b4b
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-09-30 00:30:01 -0300

    configuring default configuration yml and changing the specs to reflect this

--- lib/standalone_migrations/tasks.rb
+++ lib/standalone_migrations/tasks.rb
@@ -1,9 +1,19 @@
 module StandaloneMigrations
   class Tasks
-    def self.load_tasks
-      MinimalRailtieConfig.load_tasks
-      load "active_record/railties/databases.rake"
-      load "standalone_migrations/tasks/connection.rake"
+    class << self
+      def configure
+        config_database_file = Configurator.new.config
+        paths = Rails.application.config.paths
+        paths.add "config/database", :with => config_database_file
+      end
+
+      def load_tasks
+        configure
+
+        MinimalRailtieConfig.load_tasks
+        load "active_record/railties/databases.rake"
+        load "standalone_migrations/tasks/connection.rake"
+      end
     end
   end
 end
--- spec/standalone_migrations_spec.rb
+++ spec/standalone_migrations_spec.rb
@@ -40,7 +40,8 @@ describe 'Standalone migrations' do
     write 'Rakefile', <<-TXT
 $LOAD_PATH.unshift '#{File.expand_path('lib')}'
 begin
-  require 'tasks/standalone_migrations'
+  require "standalone_migrations"
+  StandaloneMigrations::Tasks.load_tasks
 rescue LoadError => e
   puts "gem install standalone_migrations to get db:migrate:* tasks! (Error: \#{e})"
 end
