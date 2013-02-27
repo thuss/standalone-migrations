1213c6655cc9f54c892eeb5ebce47e754fe314dc
Wes Oldenbeuving <narnach@gmail.com>
2013-02-27 13:34:13 +0100

    Support/require ActiveRecord 4.0.0.beta1
    Two files needed changes to make the specs pass.
    
    * lib/standalone_migrations.rb:
    
    	`rake db:create` was ignoring ENV['DB']. It seems like ActiveRecord now loads the DB configuration from `ENV['RAILS_ENV']` when it is required, which it did not do before. Setting `ENV['RAILS_ENV']` before requiring ActiveRecord ensures it has the intended environment.
    
    * spec/standalone_migrations_spec.rb:
    
    	Newly created migrations now use `change()` instead of `up()` and `down()`. Updated specs to expect/use the new behavior.

--- lib/standalone_migrations.rb
+++ lib/standalone_migrations.rb
@@ -1,6 +1,10 @@
 lib_path = File.expand_path("../", __FILE__)
 $:.unshift lib_path unless $:.include?(lib_path)
 
+if !ENV["RAILS_ENV"]
+  ENV["RAILS_ENV"] = ENV["DB"] || ENV["RACK_ENV"] || "development"
+end
+
 require "rubygems"
 require "rails"
 require "active_record"
@@ -13,7 +17,3 @@ APP_PATH = File.expand_path(railtie_app_path,  __FILE__)
 
 require "standalone_migrations/minimal_railtie_config"
 require "standalone_migrations/tasks"
-
-if !ENV["RAILS_ENV"]
-  ENV["RAILS_ENV"] = ENV["DB"] || ENV["RACK_ENV"] || Rails.env || "development"
-end
