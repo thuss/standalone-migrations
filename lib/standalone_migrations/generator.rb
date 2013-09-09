7ce5d7b17f0f5e403c87ce5ccaf8e08d209e6baf
Robert Lane <rlane@onemedical.com>
2013-09-09 07:44:25 -0700

    class methods to generator to move migration after the rails generator is run, removes the standard db/migrate directory on completion

--- lib/standalone_migrations/generator.rb
+++ lib/standalone_migrations/generator.rb
@@ -6,6 +6,34 @@ module StandaloneMigrations
       generator_params = [name] + options.split(" ")
       Rails::Generators.invoke "active_record:migration", generator_params,
         :destination_root => Rails.root
+      move_migration name
     end
+
+    def self.move_migration(name)
+      Dir.glob("db/migrate/*.rb").each do |migration|
+        new_path = "db/#{ENV['SOURCE']}/migrate/#{File.basename(migration)}"
+        begin
+          if migration_exists?(name, new_path)
+            raise "A migration already exists by the name of #{name}"+
+                      " for the DB source #{ENV['SOURCE']}"
+          else
+            FileUtils.mv migration, new_path
+          end
+        ensure
+          FileUtils.rm_rf "db/migrate"
+        end
+      end
+    end
+    private_class_method :move_migration
+
+    def self.migration_exists?(name, path)
+      Dir.glob("#{File.dirname(path)}/*_#{name}.rb").each do |existing_file|
+        if File.basename(existing_file) =~ /^\d+_#{name}.rb$/
+          return true
+        end
+      end
+      false
+    end
+    private_class_method :migration_exists?
   end
 end
